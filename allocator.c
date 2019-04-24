d71c85c7b2a1b9a00d98ec4681fc3c2c799748ad
hao <hshen12@dons.usfca.edu>
2019-04-24 16:17:18 -0700

    commit

--- allocator.c
+++ allocator.c
@@ -23,40 +23,41 @@
 #include <assert.h>
 #include <pthread.h>
 
+
 #ifndef DEBUG
 #define DEBUG 1
 #endif
 
 #define LOG(fmt, ...) \
-        do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
-                                __LINE__, __func__, __VA_ARGS__); } while (0)
+do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+	__LINE__, __func__, __VA_ARGS__); } while (0)
 
 #define LOGP(str) \
-    do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): %s", __FILE__, \
-            __LINE__, __func__, str); } while (0)
+do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): %s", __FILE__, \
+	__LINE__, __func__, str); } while (0)
 
 struct mem_block {
     /* Each allocation is given a unique ID number. If an allocation is split in
      * two, then the resulting new block will be given a new ID. */
-    unsigned long alloc_id;
+	unsigned long alloc_id;
 
     /* Size of the memory region */
-    size_t size;
+	size_t size;
 
     /* Space used; if usage == 0, then the block has been freed. */
-    size_t usage;
+	size_t usage;
 
     /* Pointer to the start of the mapped memory region. This simplifies the
      * process of finding where memory mappings begin. */
-    struct mem_block *region_start;
+	struct mem_block *region_start;
 
     /* If this block is the beginning of a mapped memory region, the region_size
      * member indicates the size of the mapping. In subsequent (split) blocks,
      * this is undefined. */
-    size_t region_size;
+	size_t region_size;
 
     /* Next block in the chain */
-    struct mem_block *next;
+	struct mem_block *next;
 };
 
 /* Start (head) of our linked list: */
@@ -76,35 +77,55 @@ pthread_mutex_t g_alloc_mutex = PTHREAD_MUTEX_INITIALIZER;
  */
 void print_memory(void)
 {
-    puts("-- Current Memory State --");
-    struct mem_block *current_block = g_head;
-    struct mem_block *current_region = NULL;
-    while (current_block != NULL) {
-        if (current_block->region_start != current_region) {
-            current_region = current_block->region_start;
-            printf("[REGION] %p-%p %zu\n",
-                    current_region,
-                    (void *) current_region + current_region->region_size,
-                    current_region->region_size);
-        }
-        printf("[BLOCK]  %p-%p (%ld) %zu %zu %zu\n",
-                current_block,
-                (void *) current_block + current_block->size,
-                current_block->alloc_id,
-                current_block->size,
-                current_block->usage,
-                current_block->usage == 0
-                    ? 0 : current_block->usage - sizeof(struct mem_block));
-        current_block = current_block->next;
-    }
+	puts("-- Current Memory State --");
+	struct mem_block *current_block = g_head;
+	struct mem_block *current_region = NULL;
+	while (current_block != NULL) {
+		if (current_block->region_start != current_region) {
+			current_region = current_block->region_start;
+			printf("[REGION] %p-%p %zu\n",
+				current_region,
+				(void *) current_region + current_region->region_size,
+				current_region->region_size);
+		}
+		printf("[BLOCK]  %p-%p (%ld) %zu %zu %zu\n",
+			current_block,
+			(void *) current_block + current_block->size,
+			current_block->alloc_id,
+			current_block->size,
+			current_block->usage,
+			current_block->usage == 0
+			? 0 : current_block->usage - sizeof(struct mem_block));
+		current_block = current_block->next;
+	}
 }
 
 void *reuse(size_t size) {
-    assert(g_head != NULL);
+	assert(g_head != NULL);
+
+	char *algo = getenv("ALLOCATOR_ALGORITHM");
+	if (algo == NULL) {
+		algo = "first_fit";
+	}
+
+	void *ptr = NULL;
+	if (strcmp(algo, "first_fit") == 0) {
+		first_fit_algo(size);
+	} else if (strcmp(algo, "best_fit") == 0) {
+		best_fit_algo(size, ptr);
+	} else if (strcmp(algo, "worst_fit") == 0) {
+		worst_fit_algo(size, ptr);
+	}
+
+
+
+
+
+
 
     // TODO: using free space management algorithms, find a block of memory that
     // we can reuse. Return NULL if no suitable block is found.
-    return NULL;
+	return NULL;
 }
 
 void *malloc(size_t size)
@@ -153,47 +174,55 @@ void *malloc(size_t size)
 		curr->next = block;
 	}
 
-    return block+1;
+	return block+1;
 }
 
 void free(void *ptr)
 {	
-	// print_memory();
+	print_memory();
 	// LOGP("Entering free\n");
-    if (ptr == NULL) {
+	if (ptr == NULL) {
         /* Freeing a NULL pointer does nothing */
-        return;
-    }
-    
-    struct mem_block *block = (struct mem_block*) ptr;
+		return;
+	}
+	int page_sz = getpagesize();
+	struct mem_block *block = (struct mem_block*) ptr;
+	if(block->usage == sizeof(struct mem_block)) {
+
+		LOGP("here");
+		int result = munmap(block, page_sz);
+		if(result == -1) {
+			perror("munmap");
+		}
+	}
     // TODO: free memory. If the containing region is empty (i.e., there are no
     // more blocks in use), then it should be unmapped.
-    // print_memory();
+	print_memory();
 }
 
 void *calloc(size_t nmemb, size_t size)
 {
     // TODO: hmm, what does calloc do?
-    return NULL;
+	return NULL;
 }
 
 void *realloc(void *ptr, size_t size)
 {
-    if (ptr == NULL) {
+	if (ptr == NULL) {
         /* If the pointer is NULL, then we simply malloc a new block */
-        return malloc(size);
-    }
+		return malloc(size);
+	}
 
-    if (size == 0) {
+	if (size == 0) {
         /* Realloc to 0 is often the same as freeing the memory block... But the
          * C standard doesn't require this. We will free the block and return
          * NULL here. */
-        free(ptr);
-        return NULL;
-    }
+		free(ptr);
+		return NULL;
+	}
 
     // TODO: reallocation logic
 
-    return NULL;
+	return NULL;
 }
 
